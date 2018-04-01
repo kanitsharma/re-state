dc572e2f5d037e0c31f2975dce28bc843924ca3c
Kanit Sharma <kanit.sharma@praktice.ai>
2018-04-01 21:55:27 +0530

    updated readme and added TODO

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = ({ Name, changeName }) => (
   <div className="App">
     <div>
       <h1>Hi! {Name}</h1>
-      <button onClick={() => changeName({ type: 'CHANGE_NAME', payload: { Name: 'Sharma' } })} > Change Name </button>
+      <button onClick={changeName} > Change Name </button>
     </div>
   </div>
 )
@@ -15,7 +15,7 @@ const mapStateToProps = state => ({
   Name: state.Name
 })
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   changeName: _ => dispatch({ type: 'CHANGE_NAME', payload: { Name: 'Sharma' } })
 })
 
